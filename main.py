93282fb443e08f4c79b931a78eecd433a768851d
avemoi <cmageiridis@protonmail.com>
2023-03-26 16:42:18 +0300

    fixes

--- main.py
+++ main.py
@@ -8,7 +8,8 @@ app = FastAPI()
 @app.get("/")
 async def root(time_window: int, distance_in_m: int = 20, cluster_threshold: int = 1):
     data = get_all_coordinates(time_window)
-    cluster_number = get_clusters_number(distance_in_m, data)
-    if cluster_number > cluster_threshold:
-        return {"send": 1}
+    if data:
+        cluster_number = get_clusters_number(distance_in_m, data)
+        if cluster_number > cluster_threshold:
+            return {"send": 1}
     return {"send": 0}
