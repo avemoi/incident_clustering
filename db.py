3847282356d0fc8045faa03150e5a10e034b5698
avemoi <cmageiridis@protonmail.com>
2023-03-26 13:50:10 +0300

    change db

--- db.py
+++ db.py
@@ -4,7 +4,7 @@ from sqlalchemy import create_engine, func, or_, and_, not_, MetaData
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.automap import automap_base
 
-engine = create_engine('mysql+mysqlconnector://root:mypassword@192.168.1.100:3307/lacubadb')
+engine = create_engine('mysql+mysqlconnector://root:mypassword@192.168.88.236:3308/sosprojectdb')
 # metadata = MetaData()
 # metadata.reflect(bind=engine)
 Base = automap_base()
@@ -13,9 +13,10 @@ Base.prepare(engine, reflect=True)
 #
 # Assignments = Base.classes.assignment_assignment
 #
-Lacuba = Base.classes.lacuba
+Incident = Base.classes.incident
+Users = Base.classes.users
 Session = sessionmaker(bind=engine, autocommit=False, autoflush=False)
 
 def get_all_coordinates():
     session = Session()
-    return [(coord.latitude, coord.longtitude) for coord in  session.query(Lacuba).all()]
+    return [(coord.latitude, coord.longitude) for coord in session.query(Users).all()]
